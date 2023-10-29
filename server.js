1a882a31e8955067e2b4ab16c97fefe1abf699e5
Hung Viet Hoang <hungdevngu99@gmail.com>
2023-10-29 11:57:37 +0700

    improve: release connection Db when issue happen

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const RabbitMq = require("./src/messageQueue/setupRabbitMq")
 require('dotenv').config()
-const initializeDatabase = require('./src/dbs/setupDatabase')
+const {initializeDatabase, releaseDatabaseConnection} = require('./src/dbs/setupDatabase')
 const initializeRabbitMQ = require('./src/messageQueue/setupRabbitMq')
 const {initializeWebServer} = require('./src/app')
 // Using Dependence Injection For DB and Rabbit Mq for easily testing
 async function start() {
         console.time(">>> Start WebServer")
-        await initializeDatabase( )
-        await initializeWebServer();
-        await initializeRabbitMQ(RabbitMq)
+        await initializeDatabase()
+        try {
+          await initializeWebServer();
+          await initializeRabbitMQ(RabbitMq)
+        } catch (error) {
+          releaseDatabaseConnection()
+          throw new Error("Issue happen when start webserver and message mq")
+        }
         console.timeEnd(">>> Start WebServer")
 }
 
