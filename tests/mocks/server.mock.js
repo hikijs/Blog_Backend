e296491c81bdd1ed85a4aef5d81a7bd5b8c5b159
Hung Viet Hoang <hungdevngu99@gmail.com>
2023-10-29 11:59:55 +0700

    test: Add First Test To Project
    - global-setup will init env test by start docker compose and inital DB (migrate)
    - global-teardown will restore the env like clean db but till now not working
    - docker-compose setup 3 containers mysql_test, phpmyadmin_test and rabbitmq_test
    - server was mocked to server.mock.js
    - signup.test.js is the first testcase, nock was used for isolation network and
    axious was used for http client

--- tests/mocks/server.mock.js
+++ tests/mocks/server.mock.js
@@ -1,8 +1,15 @@
-const initializeDatabase = require('../../src/dbs/setupDatabase')
-const initilizeApplication = require('../../src/app')
+const RabbitMq = require("../../src/messageQueue/setupRabbitMq")
+const {initializeDatabase, releaseDatabaseConnection} = require('../../src/dbs/setupDatabase')
+const initializeRabbitMQ = require('../../src/messageQueue/setupRabbitMq')
+const {initializeWebServer} = require('../../src/app')
+// Using Dependence Injection For DB and Rabbit Mq for easily testing
+let connection = null
 
-initializeDatabase()
+async function serverStart() {
+        await initializeDatabase()
+        const connection = await initializeWebServer();
+        await initializeRabbitMQ(RabbitMq)
+        return connection        
+}
 
-const app = initilizeApplication()
-
-module.exports = app
+module.exports = serverStart
