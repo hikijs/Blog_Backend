f094ddc352617e45ac18a26c1f7c2212fc8241b1
sieusaya149 <hungdevngu99@gmail.com>
2023-11-20 09:44:07 +0700

    improve: improve script syncstart and logging when call api

--- src/helpers/callApi.js
+++ src/helpers/callApi.js
@@ -25,7 +25,7 @@ const putApi = async (url) => {
 };
 
 const postApi = async (url, header, body) => {
-	console.log(`BE call GET api for ${url}`);
+	console.log(`BE call POST api for ${url}`);
 	try {
 		const result = await axios.post(url , body, header);
 		console.log(result);
