ca0bba55019595ce1e10447c106930b97a2f5bb5
sieusaya149 <hungdevngu99@gmail.com>
2023-12-18 16:52:13 +0700

    feature: Setup Redis For Caching
    Currently, we use the redis cloud and caching for
    the endpoint /v1/api/post/allMyPost

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const {
 	releaseDatabaseConnection,
 } = require('./src/dbs/setupDatabase');
 const initializeRabbitMQ = require('./src/messageQueue/setupRabbitMq');
+const {
+	initializeRedisCache,
+	releaseRedisConnection,
+} = require('./src/caching/setupRedis');
 const { initializeWebServer } = require('./src/app');
 // Using Dependence Injection For DB and Rabbit Mq for easily testing
 async function start() {
@@ -12,6 +16,7 @@ async function start() {
 	await initializeDatabase();
 	await initializeWebServer();
 	await initializeRabbitMQ(RabbitMq);
+	await initializeRedisCache();
 	console.timeEnd('>>> Start WebServer');
 }
 
@@ -20,6 +25,7 @@ start()
 		console.log('The app has started successfully');
 	})
 	.catch((error) => {
+		releaseRedisConnection();
 		releaseDatabaseConnection();
 		console.log('App occured during startup', error);
 	});
---
+++ src/caching/init.redis.js
@@ -0,0 +1,119 @@
+const { createClient } = require('redis');
+require('dotenv').config();
+
+// eslint-disable-next-line no-undef
+const { REDIS_HOST, REDIS_PORT, REDIS_USER, REDIS_PASSWORD } = process.env;
+
+const HOST = REDIS_HOST || '127.0.0.1';
+const PORT = REDIS_PORT || '6379';
+const USER = REDIS_USER || 'user';
+const PASSWORD = REDIS_PASSWORD || 'password';
+
+class RedisInstance {
+	constructor() {
+		this.redisUrl = `redis://${USER}:${PASSWORD}@${HOST}:${PORT}`;
+	}
+
+	static getInstance() {
+		if (!RedisInstance.instance) {
+			RedisInstance.instance = new RedisInstance();
+		}
+		return RedisInstance.instance;
+	}
+
+	async createClient() {
+		const paramsRedis = {
+			// template redis[s]://[[username][:password]@][host][:port][/db-number]
+			url: this.redisUrl,
+			socket: {
+				reconnectStrategy: (retries) => Math.min(retries * 50, 1000),
+			},
+		};
+		this.client = createClient(paramsRedis);
+		this.handlingEvent();
+		await this.client.connect();
+	}
+
+	handlingEvent() {
+		if (this.client) {
+			this.client.on('connect', () => {
+				console.log('Redis Event: connect Client Connected');
+			});
+
+			this.client.on('ready', () => {
+				console.log('Redis Event: ready Client Ready');
+			});
+
+			this.client.on('reconnecting', () => {
+				console.log(
+					'Redis Event: reconnecting Client Try To Re-Connecting'
+				);
+			});
+
+			this.client.on('end', () => {
+				console.log('Redis Event: end Connection to Redis closed');
+			});
+
+			this.client.on('error', (err) => {
+				console.log('Redis Event: Error Client Ready', err);
+			});
+		}
+	}
+
+	async closeConnection() {
+		if (this.client) {
+			await this.client.quit(() => {
+				console.log('Redis connection closed');
+			});
+		}
+	}
+
+	async setData(
+		key,
+		str,
+		opts = {
+			json: false,
+		}
+	) {
+		try {
+			if (opts.json) {
+				str = JSON.stringify(str);
+			}
+
+			if (this.client && this.client.isReady) {
+				const ttl = 20;
+				await this.client.set(key, str, {
+					EX: ttl,
+					NX: true,
+				});
+				return true;
+			}
+			return false;
+		} catch (error) {
+			console.log(`Set Data To Redis Failure ${error}`);
+			return null;
+		}
+	}
+
+	async getData(
+		key,
+		opts = {
+			json: false,
+		}
+	) {
+		try {
+			if (this.client && this.client.isReady) {
+				const data = await this.client.get(key);
+				if (data && opts.json) {
+					return JSON.parse(data);
+				}
+				return data;
+			}
+		} catch (error) {
+			console.log(`Get Data From Redis Failure ${error}`);
+			return null;
+		}
+	}
+}
+
+module.exports = RedisInstance;
