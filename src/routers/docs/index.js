cb6f987ba844dcc194a04677e06d277b5d270769
sieusaya149 <hungdevngu99@gmail.com>
2024-01-02 16:33:39 +0700

    Add Swagger Document
    - Currently Some Apis does not follow, we will update later

---
+++ src/routers/docs/index.js
@@ -0,0 +1,18 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerPath = path.join(__dirname, 'swagger.yaml');
+const swaggerDocument = YAML.load(swaggerPath);
+
+router.use(
+	'/',
+	swaggerUi.serve,
+	swaggerUi.setup(swaggerDocument, {
+		swaggerOptions: {defaultModelsExpandDepth: -1}
+	})
+);
+
+module.exports = router;
